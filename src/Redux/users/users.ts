c5bce2c7da4a85d25a551dda0ad0c962935b28e5
ali30na3m <30naali1382@gmail.com>
2024-05-17 18:40:57 +0330

    last add

--- src/Redux/users/users.ts
+++ src/Redux/users/users.ts
@@ -1,28 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
-
-<<<<<<< HEAD
-interface Todo {
-=======
 interface UserInfo {
->>>>>>> working
   id: number;
   username: string;
   password: string;
 }
 
-<<<<<<< HEAD
-interface TodosState {
-  todos: Todo[];
-}
-
-const initialState: TodosState = {
-  todos: [{ id: 1, username: "ali30na", password: 'sina1234' }],
-};
-=======
 const initialState: UserInfo[] = [
   { id: 1, username: "ali30na", password: 'sina1234' }
 ]
->>>>>>> working
 
 const todosSlice = createSlice({
   name: 'todos',
@@ -31,7 +16,3 @@ const todosSlice = createSlice({
 });
 
 export default todosSlice.reducer;
-<<<<<<< HEAD
-=======
-
->>>>>>> working
